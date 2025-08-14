6085b3066abf532b3fe3e592d0034c426c09fb03
lubaba <bsdsf22a025@pucit.edu.pk>
2025-08-14 12:31:42 +0500

    Remove bugs which occur in Login Page and Design Sign up page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PricingPage from "./pages/Pricing";
+import ForgotPassword from "./pages/ForgotPassword";
 import NotescapeStartPage from "./pages/NotescapeStartPage"; // <-- import your start page
 
 const container = document.getElementById("root") as HTMLElement;
@@ -16,6 +17,8 @@ root.render(
       {/* Home route */}
       <Route path="/" element={<NotescapeStartPage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/get-started" element={<NotescapeStartPage />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       {/* Pricing route */}
       <Route path="/pricing" element={<PricingPage />} />
     </Routes>
